644a5af6aac5b8d2f7dd357185bac11875c94c2e
Sho Takemori <stakemorii@gmail.com>
2017-07-13 16:46:34 +0200

    Add test_points_in_ellipse

--- src/test.rs
+++ src/test.rs
@@ -1,1 +1,18 @@
+extern crate gmp;
 
+use theta_chars::points_in_ellipse;
+
+#[test]
+fn test_points_in_ellipse() {
+    let r = 300;
+    assert_eq!(points_in_ellipse(0, 0, r).len(), 63205);
+    assert_eq!(points_in_ellipse(0, 1, r).len(), 63230);
+    assert_eq!(points_in_ellipse(1, 0, r).len(), 63204);
+    assert_eq!(points_in_ellipse(1, 1, r).len(), 63240);
+
+    let r = 500;
+    assert_eq!(points_in_ellipse(0, 0, r).len(), 175635);
+    assert_eq!(points_in_ellipse(0, 1, r).len(), 175636);
+    assert_eq!(points_in_ellipse(1, 0, r).len(), 175624);
+    assert_eq!(points_in_ellipse(1, 1, r).len(), 175612);
+}
