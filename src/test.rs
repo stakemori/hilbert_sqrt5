0c50dbced1c8aba2d89e8f0c6e13eaa5a944a0b6
Sho Takemori <stakemorii@gmail.com>
2017-07-14 16:53:17 +0200

    Chagne definition of points_in_ellipse

--- src/test.rs
+++ src/test.rs
@@ -2,15 +2,9 @@ use theta_chars::points_in_ellipse;
 
 #[test]
 fn test_points_in_ellipse() {
-    let r = 300;
-    assert_eq!(points_in_ellipse(0, 0, r).len(), 63205);
-    assert_eq!(points_in_ellipse(0, 1, r).len(), 63230);
-    assert_eq!(points_in_ellipse(1, 0, r).len(), 63204);
-    assert_eq!(points_in_ellipse(1, 1, r).len(), 63240);
-
-    let r = 500;
-    assert_eq!(points_in_ellipse(0, 0, r).len(), 175635);
-    assert_eq!(points_in_ellipse(0, 1, r).len(), 175636);
-    assert_eq!(points_in_ellipse(1, 0, r).len(), 175624);
-    assert_eq!(points_in_ellipse(1, 1, r).len(), 175612);
+    let r = 10000;
+    assert_eq!(points_in_ellipse(0, 0, r).len(), 15701);
+    assert_eq!(points_in_ellipse(0, 1, r).len(), 15708);
+    assert_eq!(points_in_ellipse(1, 0, r).len(), 15684);
+    assert_eq!(points_in_ellipse(1, 1, r).len(), 15716);
 }
--- src/theta_chars.rs
+++ src/theta_chars.rs
@@ -1,15 +1,14 @@
-/// Return vec of (u, v) s.t. (u^2 + 5v^2)/2 <= r^2 and u equiv a, v equiv b mod
-/// 2.
+/// Return vec of (u, v) s.t. (u^2 + 2uv + 5v^2)/4 <= r and u equiv a, v equiv b
+/// mod 2.
 pub fn points_in_ellipse(a: i64, b: i64, r: i64) -> Vec<(i64, i64)> {
     let mut vec: Vec<(i64, i64)> = Vec::new();
-    let r1 = (2_f64.sqrt() * r as f64 / 5_f64.sqrt()).floor() as i64;
-    let t_r_sq = 2 * r * r;
-    for v in -r1..(r1 + 1) {
-        let f_v_sq = 5 * v * v;
-        let r2 = ((t_r_sq - f_v_sq) as f64).sqrt().floor() as i64;
-        for u in -r2..(r2 + 1) {
-            if (u - a) & 1 == 0 && (v - b) & 1 == 0 && u * u + f_v_sq <= t_r_sq {
-                vec.push((u, v))
+    let n1 = (r as f64).sqrt().floor() as i64;
+    for y in -n1..(n1 + 1) {
+        let n2 = (2_f64 * ((r - y * y) as f64).sqrt()).floor() as i64;
+        for x in (-n2 - y)..(n2 - y + 1) {
+            debug_assert!(x * x + 2 * x * y + 5 * y * y <= 4 * r);
+            if (x - a) & 1 == 0 && (y - b) & 1 == 0 {
+                vec.push((x, y))
             }
         }
     }
