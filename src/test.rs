0135dc4df3bd7e4fbe4bd0151ce5a702027b69a0
Sho Takemori <stakemorii@gmail.com>
2017-07-20 13:51:43 +0200

    Fix points_in_ellipse

--- src/test.rs
+++ src/test.rs
@@ -3,8 +3,8 @@ use theta_chars::points_in_ellipse;
 #[test]
 fn test_points_in_ellipse() {
     let r = 10000;
-    assert_eq!(points_in_ellipse(0, 0, r).len(), 15701);
-    assert_eq!(points_in_ellipse(0, 1, r).len(), 15708);
-    assert_eq!(points_in_ellipse(1, 0, r).len(), 15684);
-    assert_eq!(points_in_ellipse(1, 1, r).len(), 15716);
+    assert_eq!(points_in_ellipse(0, 0, r).len(), 7845);
+    assert_eq!(points_in_ellipse(0, 1, r).len(), 7854);
+    assert_eq!(points_in_ellipse(1, 0, r).len(), 7842);
+    assert_eq!(points_in_ellipse(1, 1, r).len(), 7860);
 }
--- src/theta_chars.rs
+++ src/theta_chars.rs
@@ -1,8 +1,9 @@
-/// Return vec of (u, v) s.t. (u^2 + 2uv + 5v^2)/4 <= r and u equiv a, v equiv b
-/// mod 2.
+/// Return vec of (x, y) s.t. (x^2 + 2uv + 5v^2)/4 <= r and x equiv a, y equiv b
+/// mod 2, (x-a)/2 equiv (x-b)/2 mod 2.
 pub fn points_in_ellipse(a: i64, b: i64, r: i64) -> Vec<(i64, i64)> {
     let mut vec: Vec<(i64, i64)> = Vec::new();
     let n1 = (r as f64).sqrt().floor();
+    let dab = a - b;
     debug_assert!(n1 < ::std::i64::MAX as f64);
     let n1 = n1 as i64;
     for y in -n1..(n1 + 1) {
@@ -11,7 +12,7 @@ pub fn points_in_ellipse(a: i64, b: i64, r: i64) -> Vec<(i64, i64)> {
         let n2 = n2 as i64;
         for x in (-n2 - y)..(n2 - y + 1) {
             debug_assert!(x * x + 2 * x * y + 5 * y * y <= 4 * r);
-            if (x - a) & 1 == 0 && (y - b) & 1 == 0 {
+            if (x - a) & 1 == 0 && (y - b) & 1 == 0 && (x - y - dab) & 0b11 == 0 {
                 vec.push((x, y))
             }
         }
