c8bfbe09c54050e45d903f1d29455a1e593fa08a
Sho Takemori <stakemorii@gmail.com>
2017-07-21 19:18:33 +0200

    Add benches

---
+++ benches/benchmarks.rs
@@ -0,0 +1,15 @@
+#![feature(test)]
+
+extern crate test;
+extern crate hilbert_sqrt5;
+
+use hilbert_sqrt5::misc::prime_sieve;
+
+use test::Bencher;
+
+#[bench]
+fn prime_sieve_bench(b: &mut Bencher) {
+    b.iter(|| {
+        prime_sieve(10000);
+    })
+}
