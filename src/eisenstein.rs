d8ea66331906348e7bb5d0f4a5c084896c28ecd0
Sho Takemori <stakemorii@gmail.com>
2017-08-10 19:17:01 +0200

    Add f6

--- src/eisenstein.rs
+++ src/eisenstein.rs
@@ -276,6 +276,19 @@ fn split_prime_gen(p: i64) -> Sqrt5Elt<i64> {
     }
 }
 
+pub fn f6_normalized(prec: usize) -> HmfGen<Mpz> {
+    let a = Mpz::from_ui(21600);
+    let g2 = eisenstein_series(2, prec);
+    let g6 = eisenstein_series(6, prec);
+    let mut f6 = HmfGen::new(prec);
+    f6.pow_mut(&g2, 3);
+    f6 *= &Mpz::from_ui(67);
+    f6 -= &g6;
+    assert!(f6.is_divisible_by_const(&a));
+    f6 /= &a;
+    f6
+}
+    
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -296,16 +309,7 @@ mod tests {
 
     #[test]
     fn test_g6() {
-        let prec = 10;
-        let g2 = eisenstein_series(2, prec);
-        let g6 = eisenstein_series(6, prec);
-        let mut f6 = HmfGen::new(prec);
-        f6.pow_mut(&g2, 3);
-        f6 *= &Mpz::from_ui(67);
-        f6 -= &g6;
-        assert!(f6.is_divisible_by_const(&Mpz::from_ui(21600)));
-        f6 /= &Mpz::from_ui(21600);
-        assert_eq!(f6.weight, Some((6, 6)));
+        let f6 = f6_normalized(10);
         println!("{}", f6);
     }
 }
