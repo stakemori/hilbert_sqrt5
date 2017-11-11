478fd51e363002ac6295f5acdedb0e0e35b03318
Sho Takemori <stakemorii@gmail.com>
2017-11-11 19:09:48 +0100

    Use two tests

--- tests/test.rs
+++ tests/test.rs
@@ -769,7 +769,11 @@ mod str_exe {
 
     #[test]
     fn test_save_star_norms0() {
-        save_star_norms(&(36..51).collect::<Vec<_>>());
+        save_star_norms(&(36..51).filter(|i| i % 2 == 0).collect::<Vec<_>>());
+    }
+    #[test]
+    fn test_save_star_norms1() {
+        save_star_norms(&(36..51).filter(|i| i % 2 == 1).collect::<Vec<_>>());
     }
 
     fn write_csv_form(f: &HmfGen<Sqrt5Mpz>, p: &String) {
